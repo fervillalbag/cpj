dbde5592350b7edda910bc92a50b9602a3c86846
Fernando <fervillalbag@gmail.com>
2020-10-26 10:57:08 -0300

    Iniciando el proyecto

---
+++ js/app.js
@@ -0,0 +1,58 @@
+
+const buttonDropdown = document.querySelector('#buttonDropdown');
+const buttonDropdown1 = document.querySelector('#buttonDropdown1');
+const overlayNav = document.querySelector('.overlayNav');
+const dropdown = document.querySelector('#dropdown');
+const dropdown1 = document.querySelector('#dropdown1');
+
+const navbarMenu = document.querySelector('.navbar__menu');
+const navbarLinks = document.querySelectorAll('.dropdown__link');
+const navbar = document.querySelector('.navbar');
+
+const logoImage = document.querySelector('.logo__image');
+const buttonToggle = document.querySelector('#buttonToggle');
+const buttonClose = document.querySelector('#buttonClose');
+
+/**/
+buttonDropdown.addEventListener('click', (e) => {
+   e.preventDefault();
+   dropdown.classList.toggle('active');
+   overlayNav.classList.add('active');
+});
+
+buttonDropdown1.addEventListener('click', (e) => {
+   e.preventDefault();
+   dropdown1.classList.toggle('active');
+   overlayNav.classList.add('active');
+});
+
+
+overlayNav.addEventListener('click', () => {
+   dropdown.classList.remove('active');
+   dropdown1.classList.remove('active');
+   overlayNav.classList.remove('active');
+});
+
+navbarLinks.forEach(navbarLink => {
+   navbarLink.addEventListener('click', () => {
+      overlayNav.classList.remove('active');
+   });
+});
+
+window.addEventListener('scroll', (e) => {
+   if (window.scrollY > 100) {
+      navbar.classList.add('active');
+      logoImage.classList.add('active');
+   } else {
+      logoImage.classList.remove('active');
+      navbar.classList.remove('active');
+   }
+});
+
+buttonToggle.addEventListener('click', () => {
+   navbarMenu.classList.toggle('active');
+});
+
+buttonClose.addEventListener('click', () => {
+   navbarMenu.classList.remove('active');
+})
